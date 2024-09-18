061d321458d518c4a8dc116bef9078155a7100b4
Daniil Zaburunov <zaburunov.2000@mail.ru>
2024-09-19 00:03:53 +0400

    начальный этап

--- image-slider-demo/src/App.js
+++ image-slider-demo/src/App.js
@@ -1,7 +1,23 @@
 import "./App.css";
+import ImageSlider from "./ImageSlider";
 
 function App() {
-  return <div className="App"></div>;
+  
+  // const images = [
+  //   lighthouseImg,
+  //   flowerImg,
+  //   dandelion,
+  //   lighthouseImg,
+  //   flowerImg,
+  //   dandelion,
+  //   lighthouseImg,
+  //   // Add more image URLs here
+  // ];
+  return (
+    <div className="App">
+      <ImageSlider  />
+    </div>
+  );
 }
 
 export default App;
